ab8ac72d17069ac28f651dc4f83c770e0852a952
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-01-31 23:09:35 +0200

    create BookSerializer

--- backend/shelf/serializer.py
+++ backend/shelf/serializer.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+
+import models
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Book
+        fields = ['title', 'file', 'description', 'language', 'author', 'added_date', 'tags']
